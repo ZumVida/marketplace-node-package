cd284ea3334bc5e6e03550ddf63758d97eb6897b
AdriCQ <adriancapote95@gmail.com>
2023-12-31 23:36:59 -0500

    fix error

--- src/utils/crud.ts
+++ src/utils/crud.ts
@@ -1,5 +1,5 @@
 import type { AxiosInstance } from 'axios';
-import type { PaginatedData, PaginationParams } from './types';
+import type { PaginatedData, PaginationParams } from '../types';
 
 /**
  * generateCrud
