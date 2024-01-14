c8a4255d3ecaaf19953b61dddc5ca6142cf4a73a
AdriCQ <adriancapote95@gmail.com>
2024-01-14 11:53:34 -0500

    store type

--- src/types/marketplace/store.ts
+++ src/types/marketplace/store.ts
@@ -6,6 +6,7 @@ export interface Store {
   image: string | null;
   name: string;
   slug: string;
+  type: StoreType;
 }
 
 export interface StoreRequestCreate {
@@ -14,12 +15,19 @@ export interface StoreRequestCreate {
   image_url?: string;
   name: string;
   owner_id: number;
+  type?: StoreType;
 }
 
 export interface StoreRequestFilter extends PaginationParams {
   name?: string;
   owner_id?: number;
+  type?: StoreType;
   verified?: boolean;
 }
 
 export type StoreRequestUpdate = Partial<StoreRequestCreate>;
+
+export enum StoreType {
+  RESTAURANT = 'restaurant',
+  STORE = 'store',
+}
